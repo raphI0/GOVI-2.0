b4f5e213292caab0c5fa8eccfcb96212ed871b14
RaphI0 <raphdujarrier@gmail.com>
2023-06-17 10:08:24 +0200

    Update Conducteur.java

--- GOVI-Back/src/main/java/com/sncf/govi/service/model/Conducteur.java
+++ GOVI-Back/src/main/java/com/sncf/govi/service/model/Conducteur.java
@@ -1,4 +1,14 @@
 package com.sncf.govi.service.model;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class Conducteur {
+    private String residence;
+    private String codeADC;
+    private Boolean isPS;
+    private Boolean isFS;
+    private CouleurEnum couleur;
 }
